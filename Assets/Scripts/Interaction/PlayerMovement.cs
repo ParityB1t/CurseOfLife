79c8b91e2b727e93feeb4af3c309b6900b8deffc
Hung Hoang <hung.h.progdev@gmail.com>
2016-01-29 22:51:54 +0000

    player can move in 4 directions

---
+++ Assets/Scripts/Interaction/PlayerMovement.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private float speed = 0.1f;
+
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+        /*float horizontal = Input.GetAxis("Horizontal");
+	    float vertical = Input.GetAxis("Vertical");
+
+	    if (horizontal < 0)
+	    {
+            transform.position -= new Vector3(speed, 0, 0);
+        }
+	    else
+	    {
+            transform.position += new Vector3(speed, 0, 0);
+        }
+
+        if (vertical < 0)
+        {
+            transform.position -= new Vector3(speed, 0, 0);
+        }
+        else
+        {
+            transform.position += new Vector3(speed, 0, 0);
+        }*/
+
+        if (Input.GetKey(KeyCode.A))
+	    {
+            transform.position -= new Vector3(speed, 0, 0);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            transform.position += new Vector3(speed, 0, 0);
+        }
+
+	    if (Input.GetKey(KeyCode.W))
+	    {
+            transform.position += new Vector3(0, speed, 0);
+        }
+        else if (Input.GetKey(KeyCode.S))
+	    {
+            transform.position -= new Vector3(0, speed, 0);
+        }
+
+    }
+}
