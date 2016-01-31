02d6ad13113973ebb17b12256a8830d7f42ebed8
Kyle <karat28@hotmail.co.uk>
2016-01-31 09:48:38 +0000

    just a bit of heart stuff not close to completion yet.

---
+++ Assets/Heart.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Heart : MonoBehaviour {
+    public float heartBeatSpeed;
+    public bool isNormalHeartRate;
+    public GameObject heart1;
+    public GameObject heart2;
+    public bool showHeart1;
+    public bool showHeart2;
+    public StealthGameMode playerStealth;
+
+	// Use this for initialization
+	void Start () {
+        
+        heartBeatSpeed = 1;
+        heart1 = GameObject.Find("heart1");
+        heart2 = GameObject.Find("heart2");
+        
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	    
+	}
+
+    public void ShowHeart1()
+    {
+        heart1.SetActive(true);
+        heart2.SetActive(false);
+    }
+
+    public void ShowHeart2()
+    {
+        heart2.SetActive(true);
+        heart1.SetActive(false);
+    }
+}
--- Assets/Scripts/Common/engine.cs
+++ Assets/Scripts/Common/engine.cs
@@ -9,6 +9,7 @@ public class engine : MonoBehaviour {
     public GameObject Canvas;
     public GameObject Inventory;
 
+
     public GameObject TextBoxManager;
     public GameObject OpeningDialogueTrigger;
     
@@ -16,7 +17,8 @@ public class engine : MonoBehaviour {
     public GameObject worldMap;
     public GameObject Walls;
         
-
+    //heart
+    public GameObject heart;
 
     // Bosses
     public GameObject sleepBoss;
@@ -26,8 +28,8 @@ public class engine : MonoBehaviour {
 
         #region environment
 
-	    Instantiate(worldMap);
-	    Instantiate(Walls, new Vector3(0, 12), Walls.transform.rotation);
+	   // Instantiate(worldMap);
+	  //  Instantiate(Walls, new Vector3(0, 12), Walls.transform.rotation);
 
         #endregion
 
@@ -35,6 +37,7 @@ public class engine : MonoBehaviour {
         transform.SetParent(playerInstance.transform);
         GameObject textBoxManagerInstance = Instantiate(TextBoxManager);
 	    GameObject inventoryInstance = Instantiate(Inventory);
+        GameObject heartInstance = Instantiate(heart);
 
         textBoxManagerInstance.GetComponent<TextBoxManager>().player = playerInstance.GetComponent<PlayerMovement>();
         textBoxManagerInstance.GetComponent<TextBoxManager>().textBox = GameObject.FindGameObjectWithTag("textbox");
@@ -42,6 +45,12 @@ public class engine : MonoBehaviour {
         textBoxManagerInstance.GetComponent<TextBoxManager>().InitTextBox();       
 
         transform.SetParent(playerInstance.transform);
+
+        #region heart
+        Heart heartComponent = heartInstance.GetComponent<Heart>();
+        heartComponent.isNormalHeartRate = true;
+        heartComponent.playerStealth = player.GetComponent<StealthGameMode>();
+        #endregion
 
         #region inventory
 
