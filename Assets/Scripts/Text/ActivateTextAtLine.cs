291e45afefad74747860501cd16c17da8fe7d649
Kyle <karat28@hotmail.co.uk>
2016-01-31 08:36:41 +0000

    Textboxes sort of working, we need to go through together to see if we can sort it out.

---
+++ Assets/Scripts/Text/ActivateTextAtLine.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ActivateTextAtLine : MonoBehaviour {
+    public TextAsset theText;
+
+    public int startLine;
+    public int endLine;
+
+    public bool requireButtonPress;
+    private bool waitForPress;
+
+    public TextBoxManager theTextBox;
+
+    public bool destroyWhenActivated;
+
+	// Use this for initialization
+	void Start () {
+        theTextBox = FindObjectOfType<TextBoxManager>();
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (waitForPress && Input.GetKeyDown(KeyCode.KeypadEnter))
+        {
+            theTextBox.reloadScript(theText);
+            theTextBox.currentLine = startLine;
+            theTextBox.endAtLine = endLine;
+            theTextBox.EnableTextBox();
+            if (destroyWhenActivated)
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+
+    //when the player enters the collider.
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.name == "Player(Clone)")
+        {
+            if (requireButtonPress)
+            {
+                waitForPress = true;
+                return;
+            }
+            theTextBox.reloadScript(theText);
+            theTextBox.currentLine = startLine;
+            theTextBox.endAtLine = endLine;
+            theTextBox.EnableTextBox();
+            if (destroyWhenActivated)
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+
+    //if the player leaves the area.
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.name == "Player")
+        {
+            waitForPress = false;
+        }
+    }
+}
